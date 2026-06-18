0c89ba07f1fda8f92ddf234a0fa1d66ccbd68ab6
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2026-06-18 13:08:11 -0400

    commit:

--- src/Components/Main/PrintInvoiceFormate.tsx
+++ src/Components/Main/PrintInvoiceFormate.tsx
@@ -21,7 +21,7 @@ export default function PrintInvoiceFormate({ invoice }: { invoice: any }) {
     }
 
     return (
-        <div className={`print-area text-black uppercase bg-white ${roboto.variable}`}>
+        <div className={`print-area w-[50rem] text-black uppercase bg-white ${roboto.variable}`}>
             <div className="flex justify-end">
                 {invoice?.BillType == "KOT" ? (
                     <div className="flex flex-row">
