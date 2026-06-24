d2a38e7f37d220e223dbef695d6fb21b9b9236eb
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2026-06-24 14:15:35 -0400

    fix : printerName problem

--- src/lib/printer/escpos.ts
+++ src/lib/printer/escpos.ts
@@ -6,7 +6,8 @@ export async function printInvoice(invoice: any, Company: any) {
 
     await connectPrinter();
     const branchPrinterName = Company?.branch?.printerName;
-    const printer = branchPrinterName || "Star SP700 TearBar (SP712)" || "Star BSC10 (Copy 1)";
+    const printer = branchPrinterName;
+    console.log("Printer Name:", printer);
 
     const config = qz.configs.create(printer);
 
