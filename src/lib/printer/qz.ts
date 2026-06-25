5b504c5be79ebf9f0143dc454015ca4cda639eaf
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2026-06-25 14:58:54 -0400

    certificate applyfor qz

--- src/lib/printer/qz.ts
+++ src/lib/printer/qz.ts
@@ -1,10 +1,32 @@
 import qz from "qz-tray";
 
+let configured = false;
+
+function configureQZ() {
+    if (configured) return;
+
+    configured = true;
+
+    qz.security.setCertificatePromise(() =>
+        fetch("/api/qz/certificate").then((r) => r.text())
+    );
+
+    qz.security.setSignaturePromise((toSign) =>
+        fetch("/api/qz/sign", {
+            method: "POST",
+            headers: {
+                "Content-Type": "text/plain",
+            },
+            body: toSign,
+        }).then((r) => r.text())
+    );
+}
 export async function connectPrinter() {
     try {
+        configureQZ();
         if (!qz.websocket.isActive()) {
+
             await qz.websocket.connect();
-            console.log("QZ Connected");
         }
     } catch (err) {
         console.error("QZ Connection Error:", err);
