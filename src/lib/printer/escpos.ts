e727e6bce3860fd12de7a0b4f3d77067dcc95d94
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2026-06-27 07:46:17 -0400

    test:Qz certificate

--- src/lib/printer/escpos.ts
+++ src/lib/printer/escpos.ts
@@ -3,12 +3,14 @@ import { connectPrinter } from "./qz";
 import { generateInvoice } from "./invoice";
 
 export async function printInvoice(invoice: any, Company: any) {
-    console.log(Company?.branch._id);
-    console.log(invoice.branchId);
     await connectPrinter();
-    const printer = Company?.branch?.find(
-        (b: any) => b._id.toString() === invoice.branchId.toString()
-    )?.printerName;
+    const printer = Company?.branch?.find((b: any) => {
+        return (
+            b?._id?.toString() === invoice?.branchId?.toString() ||
+            b?._id?.toString() === invoice?.branchId?._id?.toString()
+        );
+    })?.printerName;
+
     const config = qz.configs.create(printer);
 
     const data = generateInvoice(invoice, Company);
